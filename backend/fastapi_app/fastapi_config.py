ded9785b1071b51bfc12a20c8b8f345679ce0b8e
gom-w <jjsg3318@gmail.com>
2025-07-24 16:56:57 +0900

    build: docker git upload 와 파일 수정

--- backend/fastapi_app/fastapi_config.py
+++ backend/fastapi_app/fastapi_config.py
@@ -1,32 +1,3 @@
-# from pydantic_settings import BaseSettings
-# from functools import lru_cache
-# from pathlib import Path
-# from dotenv import load_dotenv
-# import os
-
-# # 기본 환경 설정
-# os.environ.setdefault("ENV", "development")
-
-# # 환경 파일 로드
-# env_name = os.getenv("ENV")
-# env_path = Path(__file__).resolve().parent / f".env.{env_name}"
-# load_dotenv(dotenv_path=env_path)
-
-# class Settings(BaseSettings):
-#     debug: bool = False
-#     ENV: str = env_name
-#     API_TITLE: str = "FastAPI 기본"
-#     API_DESCRIPTION: str = "기본 설명"
-
-#     class Config:
-#         env_file = env_path
-#         env_file_encoding = "utf-8"
-
-# @lru_cache()
-# def get_settings():
-#     return Settings()
-
-
 from pydantic_settings import BaseSettings
 from functools import lru_cache
 from pathlib import Path
--- backend/fastapi_app/init_django.py
+++ backend/fastapi_app/init_django.py
@@ -16,24 +16,24 @@ def init_django():
     # 핵심 포인트: 'config' 폴더가 포함된 경로를 sys.path 에 넣음
     sys.path.insert(0, str(DJANGO_DIR))  # /app/django_app/config 를 import 가능하게
 
-    # 1. 기본 .env 파일
+    # 기본 .env 파일
     load_dotenv(DJANGO_DIR / ".env", override=False)
 
-    # 2. ENV 설정에 따라 추가 .env 로드
+    # ENV 설정에 따라 추가 .env 로드
     env = os.getenv("ENV", "development")
     env_path = DJANGO_DIR / f".env.{env}"
     if env_path.exists():
         load_dotenv(dotenv_path=env_path, override=True)
 
-    # 3. .env.local 우선순위
+    # .env.local 우선순위
     local_path = DJANGO_DIR / ".env.local"
     if local_path.exists():
         load_dotenv(dotenv_path=local_path, override=True)
 
-    # 4. settings 모듈 로딩
+    # settings 모듈 로딩
     settings_module = os.getenv("DJANGO_SETTINGS_MODULE", "config.settings.dev")
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
     print("DJANGO_SETTINGS_MODULE:", settings_module)
 
-    # 5. Django 초기화
+    # Django 초기화
     django.setup()
