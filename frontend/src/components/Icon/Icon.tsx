c123fa5671ee5b1daf9a8060ef4b2c95706c7831
gom-w <jjsg3318@gmail.com>
2025-08-27 10:19:42 +0900

    docs: add Icon.tsx

---
+++ frontend/src/components/Icon/Icon.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Sprite from "../../assets/icons/sprite.svg"
+
+export type IconName = 
+| 'eye' // 눈 아이콘
+| 'eye-off'
+| 'loading' // 로딩
+
+interface IconProps {
+    name: IconName;
+    size?: number | string;
+    className?: string;
+    color?: string
+}
+
+const Icon:React.FC<IconProps> = ({ name, size = 24, className, color = "var(--icon-color)"})=> {
+    return(
+        <svg
+            className={className}
+            width={size}
+            height={size}
+            style={{ color }}
+            role="img"
+            aria-hidden="true"
+        >
+            <use href={`${Sprite}#${name}`} />
+        </svg>
+    )
+}
+
+export default Icon
