5a00e8788edae32bc8b89d8b02b22c16493afb07
gom-w <jjsg3318@gmail.com>
2025-08-29 11:33:08 +0900

    feat: react dashboard 수정

---
+++ frontend/src/pages/Guest/Dashboard.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const GuestDashboard: React.FC = () => {
+    return <>게스트 페이지입니다.</>;
+}
+
+export default GuestDashboard
--- frontend/src/pages/Main/Main.tsx
+++ frontend/src/pages/Main/Main.tsx
@@ -1,11 +1,25 @@
 import React from "react"
+import { useUser } from "../../hooks/useUser"
+
+// 페이지 컴포넌트
+import UserDashboard from "../User/Dashboard";
+import AdminDashboard from "../Admin/Dashboard";
+import GuestDashboard from "../Guest/Dashboard";
 
 const Main: React.FC = () => {
-    return(
+
+    const {role} = useUser();
+
+    return (
         <>
             <div>⚠️ 공사중</div>
+            <div style={{ margin: "20px" }}>
+            {role === "admin" && <AdminDashboard />}
+            {role === "user" && <UserDashboard />}
+            {role === "guest" && <GuestDashboard />}
+            </div>
         </>
-    )
+    );
 }
 
 export default Main
